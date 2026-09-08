0bba0dcd6e17e0dc4c96180de85ec83cf08f3543
BlazeAkuma <deviskeyravindrarajah@gmail.com>
2026-09-08 19:00:18 +0530

    Update __root.tsx

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -76,7 +76,13 @@ function ErrorComponent({ error, reset }: { error: Error; reset: () => void }) {
 }
 
 export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()({
-  head: () => ({
+  head: () => ({scripts: [
+  {
+    src: 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1276586649400078',
+    async: true,
+    crossOrigin: 'anonymous',
+  },
+],
     meta: [
       { charSet: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
